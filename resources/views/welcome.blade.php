9968cd9551f2088f8308773eec1bf5a40d091116
keymon <1827726761@qq.com>
2017-01-22 20:41:42 +0800

    modify welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel New Year
                 </div>
 
                 <div class="links">
